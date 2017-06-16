5630b263d3f4590e6a8d45870b6cdc729147198f
liaoshiyang <965145175@qq.com>
2017-06-16 14:57:44 +0800

    git init

---
+++ StringUtils.php
@@ -0,0 +1,496 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shiyang
+ * Date: 16/5/19
+ * Time: 16:49
+ */
+
+namespace Ost\Enum;
+
+
+class StringUtils
+{
+    public static $target_img_size = 1048575;
+    static public $pic_types = array("image/gif", "image/jpeg", "image/pjpeg", "image/jpg", "image/png", "image/bmp");
+
+    public static $imageAllowFiles = [".png", ".jpg", ".jpeg", ".gif", ".bmp"]; /* 上传图片格式显示 */
+    public static $videoAllowFiles = [
+        ".flv", ".swf", ".mkv", ".avi", ".rm", ".rmvb", ".mpeg", ".mpg",
+        ".ogg", ".ogv", ".mov", ".wmv", ".mp4", ".webm", ".mp3", ".wav", ".mid"]; /* 上传视频格式显示 */
+    public static $fileAllowFiles = [
+        ".png", ".jpg", ".jpeg", ".gif", ".bmp",
+        ".flv", ".swf", ".mkv", ".avi", ".rm", ".rmvb", ".mpeg", ".mpg",
+        ".ogg", ".ogv", ".mov", ".wmv", ".mp4", ".webm", ".mp3", ".wav", ".mid",
+        ".rar", ".zip", ".tar", ".gz", ".7z", ".bz2", ".cab", ".iso",
+        ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".pdf", ".txt", ".md", ".xml"
+    ]; /* 上传文件格式显示 */
+
+    /**
+     * 字符串str中是否包含子字符串needle
+     *
+     * @param $str
+     * @param $needle
+     * @param bool $isCaseSensitive 是否区分大小写
+     * @return bool
+     */
+    public static function contain($str, $needle, $isCaseSensitive = false)
+    {
+        //检查$str是否包含$needle字符串
+        if($isCaseSensitive) {
+            return !(strpos($str, $needle) === FALSE);//注意这里的"==="
+        } else {
+            return !(stripos($str, $needle) === FALSE);//注意这里的"==="
+        }
+
+    }
+
+    /**
+     * 字符串str中是否包含单词needle(或者单词数组),
+     * 和contain方法有单词非单词的区别
+     * 如果匹配到了第一个,就停止继续匹配,如需要继续匹配则使用:preg_match_all
+     *
+     * @param $str
+     * @param $needle string or array
+     * @param bool $isCaseSensitive 是否区分大小写
+     * @return mixed
+     */
+    public static function matchWord($str, $needle, $isCaseSensitive = false)
+    {
+        if(! is_array($needle)) {
+            $needle = array($needle);
+        }
+        $words = $needle;
+        //拼装正则表达式
+        $words = array_map(function($v){
+            $v = '\\b' . $v . '\\b';
+            return $v;
+        }, $words);
+        $wordsStr = join('|', $words);
+        $iStr = $isCaseSensitive ? '' : 'i';
+        //$pattern = "/(\\bcancel\\b|\\bupdate address\\b|\\burgent\\b|\\bemergency\\b|\\bupset\\b|\\bangry\\b)/i";
+        $pattern = "/$wordsStr/$iStr";
+        preg_match($pattern, $str, $matches);
+        return $matches;
+    }
+
+    public static function startWith($str, $needle)
+    {
+        return strpos($str, $needle) === 0;
+    }
+
+    public static function endWith($str, $needle)
+    {
+        return substr($str, -strlen($needle)) === $needle;
+    }
+
+    public static function subString($str, $limit)
+    {
+        if(! empty($str)) {
+            $strLen = mb_strlen($str, 'utf-8');
+            if ($strLen >= $limit && $strLen > 1) {
+                $str = mb_substr($str, 0, $limit - 1, 'utf-8') . "...";
+            }
+        }
+        return $str;
+    }
+
+    /**
+     * 文件类型检测
+     *
+     * @param $fileName
+     * @param array $allowFiles
+     * @return bool
+     */
+    public static function checkType($fileName,Array $allowFiles)
+    {
+        return in_array(self::getFileExt($fileName), $allowFiles);
+    }
+
+    /**
+     * 获取文件扩展名
+     *
+     * @param $fileName
+     * @return string
+     */
+    public static function getFileExt($fileName)
+    {
+        return strtolower(strrchr($fileName, '.'));
+    }
+
+    public static function parseQuotationMark($keyword)
+    {
+        if (empty($keyword)) return '';
+        $keyword = trim($keyword);
+        $keyword = str_replace("'", "\\'", $keyword);
+        $keyword = str_replace('"', '\\"', $keyword);
+        $keyword = str_replace("\n", "", $keyword);//去除回车、换行
+        $keyword = str_replace("\r", "", $keyword);//去除回车、换行
+        return $keyword;
+    }
+
+    public static function parseParameter($keyword)
+    {
+
+        $parsed_keyword = preg_replace('[:cntrl:]', '', $keyword);
+
+        $parsed_keyword = str_replace("\"", " ", $parsed_keyword);
+        $parsed_keyword = str_replace("/", " ", $parsed_keyword);
+        $parsed_keyword = str_replace("\\", " ", $parsed_keyword);
+        $parsed_keyword = str_replace(":", " ", $parsed_keyword);
+        $parsed_keyword = str_replace("?", " ", $parsed_keyword);
+        $parsed_keyword = str_replace("'", " ", $parsed_keyword);
+        $parsed_keyword = str_replace("[", " ", $parsed_keyword);
+        $parsed_keyword = str_replace("]", " ", $parsed_keyword);
+        $parsed_keyword = str_replace("{", " ", $parsed_keyword);
+        $parsed_keyword = str_replace("}", " ", $parsed_keyword);
+        $parsed_keyword = str_replace(")", " ", $parsed_keyword);
+        $parsed_keyword = str_replace("(", " ", $parsed_keyword);
+        $parsed_keyword = str_replace("~", " ", $parsed_keyword);
+        $parsed_keyword = str_replace("\n", "", $parsed_keyword);//去除回车、换行
+        $parsed_keyword = str_replace("\r", "", $parsed_keyword);//去除回车、换行
+        $parsed_keyword = trim($parsed_keyword);
+        return $parsed_keyword;
+    }
+
+    public static function cleanHtml($str)
+    {
+        $str = preg_replace('/<\\s*\/?br.*?>|<\\s*\/?BR.*?>/', '', $str);
+        $str = preg_replace('/<.*?>/', '', $str);
+        $str = preg_replace('/\s+/', ' ', $str);
+        $str = preg_replace('/&nbsp;/', '', $str);
+        $str = preg_replace('[:cntrl:]', '', $str);
+        return $str;
+    }
+
+    /**
+     * 将长日期转化为短日期时间格式
+     * @param $longTime
+     * @return bool|null
+     */
+    public static function getShortTimeFromLongTime($longTime)
+    {
+        if (!isset($longTime)) {
+            return null;
+        }
+        return self::getShortTime(strtotime($longTime));
+    }
+
+    /**
+     * 将时间戳转化为短日期时间格式
+     * @param $unixtimeStamp
+     * @return bool|null
+     */
+    public static function getShortTime($unixtimeStamp)
+    {
+        if (empty($unixtimeStamp)) {
+            return null;
+        }
+        return date('Y-m-d', $unixtimeStamp);
+    }
+
+    /**
+     * 将时间戳转化为标准时间格式
+     * @param $unixtimeStamp
+     * @return bool|null
+     */
+    public static function getLongTime($unixtimeStamp)
+    {
+        if (empty($unixtimeStamp)) {
+            return null;
+        }
+        return date('Y-m-d H:i:s', $unixtimeStamp);
+    }
+
+    /**
+     * 将时间戳转化为标准时间格式
+     * @param $unixtimeStamp
+     * @return bool|null
+     */
+    public static function convertUnixTimeStamp($unixtimeStamp)
+    {
+        if (empty($unixtimeStamp)) {
+            return null;
+        }
+        return date('Y-m-d H:i:s', $unixtimeStamp);
+    }
+
+    /**
+     * 将时间格式字符串转化为时间戳
+     * @param $time
+     * @return int|null
+     */
+    public static function timeToUnixTimeStamp($time)
+    {
+        if (empty($time)) {
+            return null;
+        }
+        return strtotime($time);
+    }
+
+    /**
+     * 验证两个字符串是否相同
+     * @param string $str1
+     * @param string $str2
+     * @return bool
+     */
+    public static function validateAll($str1, $str2)
+    {
+        if ($str1 != $str2) return false;
+        return true;
+    }
+
+    /**
+     * 格式化时间
+     *
+     * @param string $time
+     * @return bool|string
+     */
+    public static function formatDate($time = 'default')
+    {
+        $date = $time == 'default' ? date('Y-m-d H:i:s', time()) : date('Y-m-d H:i:s', $time);
+        return $date;
+    }
+
+    /**
+     * 获取当前时间
+     * @return bool|string
+     */
+    public static function getCurrentDateTime()
+    {
+        return date('Y-m-d H:i:s', time());
+    }
+
+    /**
+     * 检查短日期格式是否正确
+     * @param $date string 短日期字符串
+     * @return bool|string
+     */
+    public static function checkShortDate($date)
+    {
+        //$date 这里可以任意格式
+        $is_date = strtotime($date) ? strtotime($date) : false;
+        //只要提交的是合法的日期，这里都统一成2014-11-11格式
+        return date('Y-m-d', $is_date);
+    }
+
+    /**
+     * 检查长日期格式是否正确
+     * @param $date string 长日期字符串
+     * @return bool|string
+     */
+    public static function checkLongDate($date)
+    {
+        //$date 这里可以任意格式
+        $is_date = strtotime($date) ? strtotime($date) : false;
+        //只要提交的是合法的日期，这里都统一成2014-11-11 12:12:12格式
+        return date('Y-m-d H:i:s', $is_date);
+    }
+
+    /**
+     * 验证图片类型
+     * @param string $type 图片类型
+     * @return bool
+     */
+    public static function checkImgType($type)
+    {
+        if (in_array($type, self::$pic_types)) {
+            return true;
+        }
+        return false;
+    }// 1048575=1*1024*1024 上传img大小上限1MB 单位：MB
+
+    /**
+     * 验证图片大小
+     *
+     * @param int $size 待验证文件的大小
+     * @param int $max_size 指定文件最大限制,单位：k
+     * @return bool
+     */
+    public static function checkImgSize($size,$max_size=0)
+    {
+        if(empty($max_size)) {
+            $max_size=self::$target_img_size;
+        } else {
+            $max_size=$max_size*1024;
+        }
+        return self::checkFileSize($size, $max_size);
+    }
+
+    /**
+     * 验证文件大小
+     * @param  int $size 待验证文件的大小
+     * @param  int $target 指定文件大小
+     * @return bool
+     */
+    public static function checkFileSize($size, $target = 1048575)
+    {
+        if ($size <= $target) {
+            return true;
+        }
+        return false;
+    }
+
+    public static function checkUrl($url)
+    {
+        return filter_var($url, FILTER_VALIDATE_URL);
+    }
+
+    /**
+     * 添加url参数到网址后面
+     * @param $url
+     * @param $string
+     * @return string
+     */
+    public static function addParameterToUrl($url, $string)
+    {
+        //如果$url中已经包含了$string,则直接返回
+        if (self::contain($url, $string)) {
+            return $url;
+        }
+        if (self::contain($url, "?")) {
+            $url = $url . '&' . $string;
+        } else {
+            $url = $url . '?' . $string;
+        }
+        return $url;
+    }
+
+    /**
+     * 获取客户端ip地址
+     * @return string 客户端ip
+     */
+    public static function getIP()
+    {
+        global $ip;
+        if (getenv("HTTP_CLIENT_IP"))
+            $ip = getenv("HTTP_CLIENT_IP");
+        else if (getenv("HTTP_X_FORWARDED_FOR"))
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+        else if (getenv("REMOTE_ADDR"))
+            $ip = getenv("REMOTE_ADDR");
+        else $ip = "Unknow";
+        return $ip;
+    }
+
+    /**
+     * 获取GMT标准时间 如:2015-03-14T14:38:08.531Z 在kibana上用得着
+     * @return string
+     */
+    public static function getGmdate()
+    {
+        $time = explode(" ", microtime());
+        $time2 = explode(".", $time [0] * 1000);
+        $ms = $time2 [0];//得到毫秒3位数
+        return gmdate("Y-m-d\TH:i:s.$ms\Z", strtotime("+ 8 hours"));
+    }
+
+    /**
+     * 获取带毫秒的时间 如:2015-03-14 14:38:08.531
+     * @return string
+     */
+    public static function getMillisecond()
+    {
+        $time = explode(" ", microtime());
+        $time2 = explode(".", $time [0] * 1000);
+        $ms = $time2 [0];//得到毫秒3位数
+        $time_str = self::getLongTime($time[1]) . "." . $ms;
+        return $time_str;
+    }
+
+    /**
+     * 获取前几个月的日期
+     * @param int $month
+     * @return bool|string
+     */
+    public static function getLastMonthDateTime($month = 3)
+    {
+        $month = intval($month);
+        return date('Y-m-d H:i:s',strtotime('-'.$month.' month'));
+    }
+
+    /**
+     * 获取时间差值
+     *
+     * @param int $begin_time 开始时间戳
+     * @param int $end_time 结束时间戳
+     * @return array
+     */
+    public static function timeDiff($begin_time,$end_time)
+    {
+        if($begin_time < $end_time){
+            $starttime = $begin_time;
+            $endtime = $end_time;
+        }
+        else{
+            $starttime = $end_time;
+            $endtime = $begin_time;
+        }
+        $timediff = $endtime-$starttime;
+        $days = intval($timediff/86400);
+        $remain = $timediff%86400;
+        $hours = intval($remain/3600);
+        $remain = $remain%3600;
+        $mins = intval($remain/60);
+        $secs = $remain%60;
+        $res = array("day" => $days,"hour" => $hours,"min" => $mins,"sec" => $secs);
+        return $res;
+    }
+
+    public static function arrayToObject($e)
+    {
+        if (gettype($e) != 'array') return null;
+        foreach ($e as $k => $v) {
+            if (gettype($v) == 'array' || getType($v) == 'object')
+                $e[$k] = (object)self::arrayToObject($v);
+        }
+        return (object)$e;
+    }
+
+    public static function objectToArray($e)
+    {
+        $e = (array)$e;
+        foreach ($e as $k => $v) {
+            if (gettype($v) == 'resource') return null;
+            if (gettype($v) == 'object' || gettype($v) == 'array')
+                $e[$k] = (array)self::objectToArray($v);
+        }
+        return $e;
+    }
+
+    /**
+     * 获取纯文本内容,只适用于英文等字母为基础的语种,不适合中文等
+     * 去掉所有不是字母或者数字的其他字符，用空格替代
+     *
+     * @param $str
+     * @return mixed
+     */
+    public static function getPlainText($str)
+    {
+        //去掉逗号，双引号和换行
+//        $content = str_replace('"', '', $content);
+//        $content = str_replace("'", '', $content);
+//        $content = str_replace(',', ' ', $content);
+//        $content = str_replace(PHP_EOL, '', $content);
+
+        if(empty($str)) {
+            return $str;
+        }
+        $str = self::replace4byte($str);
+        //去掉所有不是字母或者数字的其他字符，用空格替代
+        $pattern = '/\W+/';
+        $str = preg_replace($pattern,' ', $str);
+        return $str;
+    }
+
+    //去掉表情符号
+    public static function replace4byte($str)
+    {
+        if(empty($str)) return $str;
+        return preg_replace('%(?:
+          \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+        | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+        | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+    )%xs', '', $str);
+    }
+}
---
+++ attache_package.php
@@ -0,0 +1,265 @@
+<?php
+/**
+ *
+ * @authors Shiyang
+ * @date    2016-07-01 16:15:26
+ */
+//require_once __DIR__ .'/_init.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+use Ost\Enum\StringUtils;
+
+class AttachePackage
+{
+    private $count = 0;
+    private $isRealDelete = false;
+    public $dateLimit = '';
+    function __construct()
+    {
+        //记录总数
+        $this->count = 0;
+        //默认移除过去一年的附件
+        $this->dateLimit = date('Ym',strtotime('-12 month'));
+    }
+
+    function main()
+    {
+        //记录任务执行开始时间
+        $startTimeUnix = time();
+        $start_time = date('Y-m-d H:i:s',$startTimeUnix);
+        echo 'start_time:'.$start_time . "\n";
+//        $this->dateLimit = '201606';
+
+
+        //原始文件路径
+        $path = '/data/osticket_attachments/osticket';
+//        $path = '/Users/shiyang/tmp/php_test';
+
+        //需要将原始文件移动到该路径,方便后续zip打包
+        $moveToBaseDir = '/data/osticket_attachments/osticket_bak';
+        //开始移动文件到打包区
+        self::moveFiles($path, $moveToBaseDir, strlen("$path/"));
+        //按年月打包输出的根路径
+//        $outZipPath = '/tmp/Downloadsbak/';
+//        $sourcePath = $moveToBaseDir;
+//        self::zipByMouth($sourcePath, $outZipPath);
+
+        //记录任务执行结束时间
+        $endTimeUnix = time();
+        $end_time = date('Y-m-d H:i:s', $endTimeUnix);
+        $diff_time = StringUtils::timeDiff($startTimeUnix, $endTimeUnix);
+        echo 'count:'.$this->count . "\n";
+        echo 'end time:'.$end_time . "\n";
+        echo 'diff time:'.json_encode($diff_time) . "\n";
+    }
+
+    /**
+     * 移动原始目录下的所有文件到指定目录,
+     * 并以文件的写入时间作为基准判断,按年月自动建立子目录分开存储
+     *
+     * @param string $sourcePath 原始目录
+     * @param string $moveTo 移动文件到该目录
+     * @param int $exclusiveLength 排除指定位数路径截取,方便子目录操作
+     */
+    function moveFiles($sourcePath, $moveTo, $exclusiveLength)
+    {
+        //如果目录不存在,则创建该目录
+        self::mkDir($moveTo);
+        $files = new FilesystemIterator($sourcePath,FilesystemIterator::SKIP_DOTS);
+        foreach($files as $file) {
+            $fileName = $file->getFilename();
+            $filePath = $file->getPathname();
+            $localPath = substr($filePath, $exclusiveLength);
+            $localPath = str_replace($fileName, '', $localPath);
+            if($file->isDir()) {
+                $subPath = rtrim($sourcePath, '/') . '/' . $fileName;
+                $this->moveFiles($subPath, $moveTo, $exclusiveLength);
+            } else {
+                //获取文件 Modified time:在写入文件时随文件内容的更改而更改，是指文件内容最后一次被修改的时间
+                $cTime = $file->getMTime();
+                $newFolder = date('Ym',$cTime);
+//                echo 'modify date:'.$newFolder.', file name:'. $fileName . "\n";
+                if($newFolder < $this->dateLimit) {
+                    $moveToTargetDir = rtrim($moveTo, '/') . '/' .$newFolder . '/' . $localPath;
+                    //如果目录不存在,则创建该目录
+                    self::mkDir($moveToTargetDir);
+                    $moveToDirTargetFile = rtrim($moveToTargetDir, '/') . '/' . $fileName;
+                    //移动文件到指定目录
+                    rename($filePath, $moveToDirTargetFile);
+//                copy($filePath, $moveToDirTargetFile);
+                    $this->count++;//统计file总数
+                }
+            }
+
+        }
+    }
+
+    function zipByMouth($sourcePath, $outZipBasePath)
+    {
+        //如果目录不存在,则创建该目录
+        self::mkDir($outZipBasePath);
+        $files = new FilesystemIterator($sourcePath,FilesystemIterator::SKIP_DOTS);
+        foreach($files as $file) {
+            //前提是目录才进行打zip包,目录命名格式:yyyymm(如:201607)
+            if($file->isDir()) {
+                $fileName = $file->getFilename();
+                $filePath = $file->getPathname();
+                //拼接输出zip包名
+                $outZipPath = rtrim($outZipBasePath, '/') . '/' . $fileName  . '.zip';
+                HZip::zipDir($filePath, $outZipPath);
+                //压缩完成后删除原文件,腾出磁盘空间.
+                $success = self::delDir($filePath, $this->isRealDelete);
+                if($success) {
+                    echo 'delete files success! path:' . $filePath . ', time:' . StringUtils::getCurrentDateTime() . "\n";
+                } else {
+                    echo 'delete files failure! path:' . $filePath . ', time:' . StringUtils::getCurrentDateTime() . "\n";
+                }
+            }
+        }
+    }
+
+    function checkFileMTime($sourcePath)
+    {
+        $files = new FilesystemIterator($sourcePath,FilesystemIterator::SKIP_DOTS);
+        foreach($files as $file) {
+            //前提是目录才进行打zip包,目录命名格式:yyyymm(如:201607)
+            if($file->isDir()) {
+                self::checkFileMTime($file->getPathname());
+            } else {
+                //获取文件 Modified time:在写入文件时随文件内容的更改而更改，是指文件内容最后一次被修改的时间
+                $aTime = $file->getATime();
+                $mTime = $file->getMTime();
+                $cTime = $file->getCTime();
+                echo date('Y-m-d H:i:s', $aTime). ' , '. $file->getFilename() . "\n";
+                echo date('Y-m-d H:i:s', $mTime). ' , '. $file->getFilename() . "\n";
+                echo date('Y-m-d H:i:s', $cTime). ' , '. $file->getFilename() . "\n";
+            }
+        }
+    }
+
+    /**
+     * 如果目录不存在,则创建该目录
+     *
+     * @param $path
+     */
+    function mkDir($path)
+    {
+        //如果目录不存在,则创建该目录
+        if(! file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+    }
+
+    /**
+     * 遍历删除文件夹以及文件夹下的所有文件
+     *
+     * @param $path
+     * @param bool $isRealDelete
+     * @return bool
+     */
+    function delDir($path, $isRealDelete = true)
+    {
+        if(! $isRealDelete) {
+            return false;
+        }
+        //遍历删除子目录
+        $files = new FilesystemIterator($path,FilesystemIterator::SKIP_DOTS);
+        foreach($files as $file) {
+            $filePath = $file->getPathname();
+            if($file->isDir()) {
+                self::delDir($filePath, $isRealDelete);
+            } else {
+                unlink($filePath);
+            }
+        }
+        //删除当前文件夹：
+        if(rmdir($path)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+
+    /**
+     * 删除文件
+     *
+     * @param $filePath
+     */
+    function delFile($filePath)
+    {
+        $isDel = unlink($filePath);
+        if($isDel) {
+            echo 'isDel:'.$isDel . ',file path:'. $filePath . "\n";
+        }
+    }
+
+    /**
+     * 删除文件夹
+     *
+     * @param $dir
+     */
+    function delFileDir($dir)
+    {
+        $isRmDir = rmdir($dir);
+        if($isRmDir) {
+            echo 'isRmdir:'.$isRmDir . ',dir path:'. $dir . "\n";
+        }
+    }
+}
+
+$obj = new AttachePackage();
+$obj->main();
+
+class HZip
+{
+
+    /**
+     * Add files and sub-directories in a folder to zip file.
+     * @param string $folder
+     * @param ZipArchive $zipFile
+     * @param int $exclusiveLength Number of text to be exclusived from the file path.
+     */
+    private static function folderToZip($folder, ZipArchive &$zipFIle, $exclusiveLength)
+    {
+        //遍历当前目录所有文件和子目录
+        $files = new FilesystemIterator($folder,FilesystemIterator::SKIP_DOTS);
+        foreach($files as $file) {
+            $filePath = $file->getPathname();//当前文件路径
+            $localPath = substr($filePath, $exclusiveLength);
+            if($file->isDir()) {
+                //is dir
+                $zipFIle->addEmptyDir($localPath);//新建子目录
+                self::folderToZip($filePath, $zipFIle, $exclusiveLength);
+            } else {
+                // is file
+                $zipFIle->addFile($filePath, $localPath);
+            }
+
+        }
+
+    }
+
+    /**
+     * Zip a folder (include itself).
+     * Usage:
+     *   HZip::zipDir('/path/to/sourceDir', '/path/to/out.zip');
+     *
+     * @param string $sourcePath Path of directory to be zip.
+     * @param string $outZipPath Path of output zip file.
+     */
+    public static function zipDir($sourcePath, $outZipPath)
+    {
+        $pathInfo = pathInfo($sourcePath);
+        $parentName = $pathInfo['dirname'];//父级目录名称
+        $dirName = $pathInfo['basename'];//当前目录的名称
+
+        $z = new ZipArchive();
+        $z->open($outZipPath, ZipArchive::CREATE);
+        $z->addEmptyDir($dirName);
+        self::folderToZip($sourcePath, $z, strlen("$parentName/"));
+        $z->close();
+    }
+
+}
